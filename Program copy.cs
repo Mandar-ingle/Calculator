0e41d7787ce885b5b926b555c0de96c66f91840e
Mandar Ingle <mandaringle33@gmail.com>
2021-07-07 20:44:50 +0530

    first commit

---
+++ Program copy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            double firstnumber;
+            double secondnumber;
+            double answer;
+            String operation;
+
+            Console.WriteLine("Calculator");
+            Console.WriteLine("*************************");
+
+            Console.WriteLine("Enter first number\n");
+            firstnumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("select operation:(+,-,*,/,%,^)\n");
+            operation = Console.ReadLine();
+
+            Console.WriteLine("Enter second number\n");
+            secondnumber = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (operation=="+")
+            {
+                answer = firstnumber + secondnumber;
+                Console.Write("="+answer);
+            }
+            if (operation == "-")
+            {
+                answer = firstnumber - secondnumber;
+                Console.Write("=" + answer);
+            }
+            if (operation == "*")
+            {
+                answer = firstnumber * secondnumber;
+                Console.Write("=" + answer);
+            }
+            if (operation == "/")
+            {
+                answer = firstnumber / secondnumber;
+                Console.Write("=" + answer);
+            }
+            if (operation == "%")
+            {
+                answer = firstnumber % secondnumber;
+                Console.Write("=" + answer);
+            }
+            Console.Write("\n" + "press key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
